d0a1d7692508530918c92e6014d0aacbd4138f72
Michael Moncrief <mtm88001@gmail.com>
2019-03-30 01:53:12 -0500

    Enable S_CURVE_ACCELERATION, disable ZProbe, set Esteps to 274

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -624,7 +624,7 @@
  * Override with M92
  *                                      X, Y, Z, E0 [, E1[, E2[, E3[, E4]]]]
  */
-#define DEFAULT_AXIS_STEPS_PER_UNIT   { 50, 50, 800, 274 }
+#define DEFAULT_AXIS_STEPS_PER_UNIT   { 115, 115, 400, 139 }
 
 /**
  * Default Max Feed Rate (mm/s)
@@ -674,7 +674,7 @@
  *
  * See https://github.com/synthetos/TinyG/wiki/Jerk-Controlled-Motion-Explained
  */
-//#define S_CURVE_ACCELERATION
+#define S_CURVE_ACCELERATION
 
 //===========================================================================
 //============================= Z Probe Options =============================
@@ -690,7 +690,7 @@
  *
  * Enable this option for a probe connected to the Z Min endstop pin.
  */
-#define Z_MIN_PROBE_USES_Z_MIN_ENDSTOP_PIN
+//#define Z_MIN_PROBE_USES_Z_MIN_ENDSTOP_PIN
 
 /**
  * Z_MIN_PROBE_ENDSTOP
